330dc819bcea09a732e707f453a9286023653125
Gui Seek <guiseek@gmail.com>
2023-12-16 03:36:37 -0300

    fix(template): 🐛 test to release

--- packages/template/src/lib/template.spec.ts
+++ packages/template/src/lib/template.spec.ts
@@ -1,7 +1,14 @@
 import { template } from './template';
 
+class Input extends HTMLInputElement {}
+customElements.define('test-input', Input, { extends: 'input' });
+
 describe('template', () => {
   it('should work', () => {
     expect(template('span', {})).toBeInstanceOf(HTMLSpanElement);
+  });
+
+  it('should input work', () => {
+    expect(template(Input, {})).toBeInstanceOf(Input);
   });
 });
